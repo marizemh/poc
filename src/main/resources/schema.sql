a072514ffa7fa03da771909d902671c095a1ead0
Marize Mijares Hernandez <mmijares@nisum.com>
2024-03-28 17:01:24 -0400

    All files personas

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,11 @@ create table pais(
     primary key (id)
 );
 
+CREATE TABLE persona (
+  id INT NOT NULL AUTO_INCREMENT,
+  nombres VARCHAR(255) NOT NULL,
+  apellidos VARCHAR(255) NOT NULL,
+  pais VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
